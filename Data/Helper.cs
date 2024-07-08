b297a21a7dbb1caec298f837115c84b85352c670
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-07-08 13:33:48 -0400

    Creates final version

---
+++ Data/Helper.cs
@@ -0,0 +1,38 @@
+﻿namespace ContactBook;
+
+public class Helper
+{
+  public static List<Contact> GetContacts () 
+  {
+    List<Contact> contacts = [];
+
+    Contact contact = new Contact
+    {
+      FirstName = "John",
+      LastName = "Doe",
+      Email = "john.doe@email.com",
+      IsFavorite = false
+    };
+    contacts.Add(contact);
+
+    contact = new Contact 
+    {
+      FirstName = "Jane",
+      LastName = "Doe",
+      Email = "jane.doe@email.com",
+      IsFavorite = true
+    };
+    contacts.Add(contact);
+
+    contact = new Contact 
+    {
+      FirstName = "John",
+      LastName = "Smith",
+      Email = "john.smith@email.com",
+      IsFavorite = false
+    };
+    contacts.Add(contact);
+
+    return contacts;
+  }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactBook;
+
+public class Contact 
+{
+  public string? FirstName { get; set; }
+  public string? LastName { get; set; }
+  public string? Email { get; set; }
+  public bool IsFavorite { get; set; }
+}
